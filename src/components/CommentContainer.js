62a593479ffb142ce7bd734f8d6b355b5adfa965
github username <github email address>
2020-08-11 22:03:31 -0400

    unused fuke

--- src/components/CommentContainer.js
+++ src/components/CommentContainer.js
@@ -1,54 +1,54 @@
-import React from 'react'
-import { Card, Button } from 'semantic-ui-react'
-import { useSelector, useDispatch } from 'react-redux'
-import { deleteCommentAction, updateCommentAction } from '../store/comment/actions'
-import TaskDetail from './TaskDetail'
+// import React from 'react'
+// import { Card, Button } from 'semantic-ui-react'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { deleteCommentAction, updateCommentAction } from '../store/comment/actions'
+// import TaskDetail from './TaskDetail'
 
-const CommentContainer = () => {
+// const CommentContainer = () => {
   
 
-  const comments = useSelector(state => state.comment.comments)
+//   const comments = useSelector(state => state.comment.comments)
   
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchComments())
-  }, [dispatch])
+//   const dispatch = useDispatch()
+//   useEffect(() => {
+//     dispatch(fetchComments())
+//   }, [dispatch])
   
  
 
 
 
-  const handleDelete = (id) => {
-    dispatch(deleteCommentAction(id))
-  }
+//   const handleDelete = (id) => {
+//     dispatch(deleteCommentAction(id))
+//   }
 
-  // const handleUpdateButton = (id) => {
-  //   console.log("ID", id)
-  //   const taskObj = { description: "hello"}
-  //   dispatch(updateCommentAction(id, taskObj))
-  // }
-
-  
-    
-  }
-
-  return(
-    <>
-    <p></p>
-    {task.created_by === currentUser && <i className="trash icon" onClick={() => handleDelete(comment.id)} ></i>}
-{/*   <Button key={task.id} negative onClick={() => handleDeleteButton(task.id)}>X</Button> : null} */}
-      {/*  {task.created_by === currentUser ? <Button key={task.id} onClick={() => handleUpdateButton(task.id)}>Update task</Button> : null} */}
-
-    </>
-
-
-
-    
-    
-    )
-    
+//   // const handleUpdateButton = (id) => {
+//   //   console.log("ID", id)
+//   //   const taskObj = { description: "hello"}
+//   //   dispatch(updateCommentAction(id, taskObj))
+//   // }
 
   
-}
+    
+//   }
 
-export default CommentContainer;
+//   return(
+//     <>
+//     <p></p>
+//     {task.created_by === currentUser && <i className="trash icon" onClick={() => handleDelete(comment.id)} ></i>}
+// {/*   <Button key={task.id} negative onClick={() => handleDeleteButton(task.id)}>X</Button> : null} */}
+//       {/*  {task.created_by === currentUser ? <Button key={task.id} onClick={() => handleUpdateButton(task.id)}>Update task</Button> : null} */}
+
+//     </>
+
+
+
+    
+    
+//     )
+    
+
+  
+// }
+
+// export default CommentContainer;
