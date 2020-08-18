2c4696cc1857da5bb9538840ba9fb4c1e9802783
github username <github email address>
2020-08-18 12:59:32 -0400

    removing unused code

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -16,35 +16,7 @@ const TaskForm = ({ categoryId }) => {
     name: "",
     description: ""
   })
-
-  // const handleClickOutside = e => {
-  //   console.log("clicking anywhere");
-  //   console.log(e.target)
-  //   console.log(node.current)
-  //   // if (node.current.contains(e.target)) {
-  //   //   // inside click
-  //   //   return;
-  //   // }
-  //   // outside click
-  //   setIsOpen(false);
-  // };
-
-  // useEffect(() => {
-  //   if (isOpen) {
-  //     document.addEventListener("mousedown", handleClickOutside);
-  //   } else {
-  //     document.removeEventListener("mousedown", handleClickOutside);
-  //   }
-
-  //   return () => {
-  //     document.removeEventListener("mousedown", handleClickOutside);
-  //   };
-  // }, [isOpen]);
-
-
-  
-
-  
+ 
 
   const dispatch = useDispatch()
 
@@ -52,17 +24,6 @@ const TaskForm = ({ categoryId }) => {
 
     setTaskInput({...taskInput, [e.target.name]: e.target.value })
   }
-
-  // const  handleDropdownClick = (categoryId) => {
-  //   const category = categories.find(c => c.name === e.target.textContent)
-  //   setTaskInput({...taskInput, category_id: category.id })
-  //   console.log(e.target)
-  //   console.log(e.target.id)
-  // }
-
-  // const handleSelect = (categoryId) => {
-    // setTaskInput({...taskInput, category_id: categoryId })
-  // }
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -72,13 +33,6 @@ const TaskForm = ({ categoryId }) => {
     console.log(taskObj)
     dispatch(addTaskAction(taskObj))
     setIsOpen(false)
-
-    // props.handleAddTask(taskInput)
-
-    // props.handleLogin(state)
-    // TODO: make a fetch request to login the current user
-    // then set that user in state in our App component
-
   }
 
   const handleClose = () => {
@@ -86,29 +40,7 @@ const TaskForm = ({ categoryId }) => {
   }
 
     return (
-    // <Form >
-    //     <Form.Group widths='equal'>
-    //       <Form.Input fluid label='name' name="name" placeholder='Task name' onChange={handleChange} />
-    //       <Form.Input fluid label='description' name="description" placeholder='description' onChange={handleChange} />
-    //       {/* <Form.Field label="date" type={date}/> */}
-    //         {/* <label>Date</label> */}
-    //         {/* <input type="date">
-    //       </Form.Field> */}
-            
-    //       {/* /> */}
-
-    //       <Form.Input fluid label='category'>
-    //         <CategoryDropdown categories={categories} handleSelect={handleDropdownClick}/>
-    //         </Form.Input>
-
-    // </Form.Group>
-          
-    //     <Form.Button>Submit</Form.Button>
-    //   </Form>
-
-
-
-      <Popup
+       <Popup
         ref={node}
         trigger={<Button icon='add' content='Add a task' onClick={() => setIsOpen(!isOpen)} />}
         content={<Form onSubmit={handleSubmit}>
